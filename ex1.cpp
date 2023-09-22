022eafbf0af395aeffc8035d95d9a46cf4c1b0e0
Cocobunaux <corentin.bunaux@gmail.com>
2023-09-22 15:07:53 +0200

    plus de vert

--- ex1.cpp
+++ ex1.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+class Rationnel
+{
+private:
+    int num, den;
+
+public:
+    Rationnel()
+    {
+        this->den = 1;
+    }
+    Rationnel();
+};
+
 int main(void)
 {
 
